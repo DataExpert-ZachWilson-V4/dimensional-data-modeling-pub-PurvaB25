7e7a59b0f1c70ba22ddefa8225fef68e60e04c12
Purva Badve <100107332+PurvaB25@users.noreply.github.com>
2024-05-21 20:44:45 -0700

    query_4.sql

--- submission/query_4.sql
+++ submission/query_4.sql
@@ -23,7 +23,7 @@ WITH
       ) AS last_year_quality_class,
       current_year
     FROM
-      nba_players
+      actors
     WHERE
 -- Filter the records to include seasons up to and including 2021
       current_season <= 2021
