e8fff18009757a8dd4d74e35344e131be9ed03e9
Purva Badve <100107332+PurvaB25@users.noreply.github.com>
2024-05-21 20:15:12 -0700

    query_4.sql

---
+++ submission/query_4.sql
@@ -0,0 +1,74 @@
+INSERT INTO
+  actors_history_scd
+WITH
+  lagged AS (
+    SELECT
+      actor,
+      CASE
+        WHEN is_active THEN 1
+        ELSE 0
+      END AS is_active,
+-- Use LAG to get the is_active status of the previous year
+      CASE
+        WHEN LAG(is_active, 1) OVER (
+          PARTITION BY actor
+          ORDER BY current_year
+        ) THEN 1
+        ELSE 0
+      END AS is_active_last_year,
+      quality_class,
+      LAG(quality_class, 1) OVER (
+        PARTITION BY actor
+        ORDER BY current_year
+      ) AS last_year_quality_class,
+      current_year
+    FROM
+      nba_players
+    WHERE
+-- Filter the records to include seasons up to and including 2021
+      current_season <= 2021
+  ),
+  streaked AS (
+    SELECT
+      *,
+      SUM(
+        CASE
+--If is_active or quality_class varies, the streak identifier should be increased.
+          WHEN is_active <> is_active_last_year OR quality_class <> last_year_quality_class THEN 1
+          ELSE 0
+        END
+      ) OVER (
+        PARTITION BY actor
+        ORDER BY current_year
+      ) AS streak_identifier
+    FROM
+      lagged
+  ),
+  periods AS (
+    SELECT
+      actor,
+      MAX(is_active) = 1 AS is_active,
+      MIN(current_year) AS start_year,
+      MAX(current_year) AS end_year,
+-- Set the current year as 2021 for all records in this backfill
+      2021 AS current_year,
+      quality_class
+    FROM
+      streaked
+    GROUP BY
+      actor,
+      streak_identifier,
+      quality_class
+  )
+SELECT
+  actor,
+  is_active,
+  start_year AS start_date,
+  end_year AS end_date,
+  current_year,
+  quality_class
+FROM
+  periods
+ORDER BY
+  actor,
+  start_date
